f04c4f898aaba791a6dc77d5250eba2d9538fa50
q'w <q'we>
2019-08-07 23:59:53 +0800

    基础功能

--- assets/Script/rocker.ts
+++ assets/Script/rocker.ts
@@ -28,9 +28,7 @@ export default class rocker extends cc.Component {
             this.Rocker.active = false
         }
 
-        this.rocker_p = this.Rocker.position
-        this.stick_p = this.node.convertToNodeSpaceAR(this.Rocker.position)
-        this.play_p = this.play.position
+		cc.director.getScheduler().schedule(this.updatePostion, this, 0.05, false)
     }
 
     start () {
@@ -45,24 +43,18 @@ export default class rocker extends cc.Component {
    
         this.node.on(cc.Node.EventType.TOUCH_MOVE,function(e: any){
             let touch_p = this.node.convertToNodeSpaceAR(e.getLocation())
+			let p = this.rocker_p
+			
+			let x = touch_p.x - p.x
+			let y = touch_p.y - p.y
+			
+			let len = x * x + y * y
+			if (len > this.Max_r * this.Max_r) {
+				touch_p.x = x / Math.sqrt(len) * this.Max_r
+				touch_p.y = y / Math.sqrt(len) * this.Max_r
+			}
 
-            console.log(touch_p)
-            
-            if (touch_p.x == NaN || touch_p.y == NaN || this.Rocker.x == NaN || this.Rocker.y == NaN) {
-                return
-            }
-            //  180 145  227 215
-            let k = Math.abs(touch_p.y - this.Rocker.y) / (touch_p.x - this.Rocker.x)
-            let a = touch_p
-            a.x  = (Math.sqrt(1 / (k * k + 1)) + this.Rocker.x)
-            a.y = (k * Math.sqrt(1 / (k * k + 1)) + this.Rocker.y)
-            console.log(this.rocker_p)
-
-            
-
-            console.log(a)
-
-            this.stick.setPosition(a)
+            this.stick.setPosition(touch_p)
         },this);
    
         this.node.on(cc.Node.EventType.TOUCH_END,function(e: any){
@@ -72,19 +64,18 @@ export default class rocker extends cc.Component {
         },this);
     }
 
-    update () {
+    updatePostion () {
         this.rocker_p = this.Rocker.position
-        this.stick_p = this.node.convertToNodeSpaceAR(this.Rocker.position)
-        this.play_p = this.node.convertToNodeSpaceAR(this.play.position)
-        // if (Math.abs(rocker_p.x - stick_p.x) < 1 || Math.abs(rocker_p.y - stick_p.y) < 1) {
-        //     return 
-        // }
-
-        // let x = stick_p.x - rocker_p.x
-        // let y = stick_p.y - rocker_p.y
-        // let p_x = play_p.x + x * this.speed
-        // let p_y = play_p.y + y * this.speed
-
-        // this.play.setPosition(new cc.Vec2(p_x,p_y))
+        this.stick_p = this.stick.position
+        this.play_p = this.play.position
+        
+		if (this.Rocker.active == true) {
+			let s = this.stick_p
+			let p = this.play_p
+			p.x += s.x * this.speed
+			p.y += s.y * this.speed
+			//console.log(s.x,s.y)
+			this.play.setPosition(p)
+		}  
     }
 }
